77aee1c0ffa133292b054249ba8e95f7b8fef71d
pinyin <bob432091@gmail.com>
2018-06-19 19:03:03 +0800

    more tests

--- src/provide-consume.test.tsx
+++ src/provide-consume.test.tsx
@@ -11,7 +11,7 @@ describe(`${provide.name}-${consume.name}`, () => {
     const c2: string = 'str'
     const C2 = React.createContext(c2)
 
-    const c3 = () => nothing
+    const c3: () => any = () => nothing
     const C3 = React.createContext(c3)
 
     const Provides = provide(C1.Provider, C2.Provider, C3.Provider)
@@ -27,9 +27,29 @@ describe(`${provide.name}-${consume.name}`, () => {
         expect(renderer.root.type).toEqual(Consumes)
     })
 
-    test(`consumer should receive context contents as array`, () => {
+    test(`consumer should receive default contents when no provider is present`, () => {
         const renderer = create(<Consumes>{(c1, c2, c3) => `${c1}, ${c2}, ${c3}`}</Consumes>)
         expect(renderer.root.children).toEqual([`${c1}, ${c2}, ${c3}`])
+    })
+
+    test(`consumer should receive providers' contents as an array`, () => {
+        const renderer = create(
+            <Provides values={[1, 's', () => 1]}>
+                <Consumes>{(c1, c2, c3) => `${c1}, ${c2}, ${c3}`}</Consumes>
+            </Provides>
+        )
+        expect(renderer.root.findAllByType(Consumes)[0].children).toEqual([`${1}, ${'s'}, ${() => 1}`])
+    })
+
+    test(`consumer should receive mixed contents when some contexts are not provided`, () => {
+        const PartialProvides = provide(C1.Provider, C3.Provider)
+
+        const renderer = create(
+            <PartialProvides values={[1, () => 1]}>
+                <Consumes>{(c1, c2, c3) => `${c1}, ${c2}, ${c3}`}</Consumes>
+            </PartialProvides>
+        )
+        expect(renderer.root.findAllByType(Consumes)[0].children).toEqual([`${1}, ${'str'}, ${() => 1}`])
     })
 
 })
