3ab21e8f10d0b7319f3a0cc6b120a350430c5220
pinyin <bob432091@gmail.com>
2018-06-19 20:45:32 +0800

    add overlooooooooooooooads

--- src/consume.tsx
+++ src/consume.tsx
@@ -3,8 +3,14 @@ import {ComponentType, Consumer, default as React, ReactNode} from 'react'
 
 export function consume<C1>(c1: Consumer<C1>): ResultComponent<C1>
 export function consume<C1, C2>(c1: Consumer<C1>, c2: Consumer<C2>): ResultComponent<C1, C2>
-export function consume<C1, C2, C3>(c1: Consumer<C1>, c2: Consumer<C2>,
-                                    c3: Consumer<C3>): ResultComponent<C1, C2, C3>
+export function consume<C1, C2, C3>(c1: Consumer<C1>, c2: Consumer<C2>, c3: Consumer<C3>): ResultComponent<C1, C2, C3>
+export function consume<C1, C2, C3, C4>(c1: Consumer<C1>, c2: Consumer<C2>, c3: Consumer<C3>, c4: Consumer<C4>): ResultComponent<C1, C2, C3, C4>
+export function consume<C1, C2, C3, C4, C5>(c1: Consumer<C1>, c2: Consumer<C2>, c3: Consumer<C3>, c4: Consumer<C4>, C5: Consumer<C5>): ResultComponent<C1, C2, C3, C4, C5>
+export function consume<C1, C2, C3, C4, C5, C6>(c1: Consumer<C1>, c2: Consumer<C2>, c3: Consumer<C3>, c4: Consumer<C4>, C5: Consumer<C5>, C6: Consumer<C6>): ResultComponent<C1, C2, C3, C4, C5, C6>
+export function consume<C1, C2, C3, C4, C5, C6, C7>(c1: Consumer<C1>, c2: Consumer<C2>, c3: Consumer<C3>, c4: Consumer<C4>, C5: Consumer<C5>, C6: Consumer<C6>, C7: Consumer<C7>): ResultComponent<C1, C2, C3, C4, C5, C6, C7>
+export function consume<C1, C2, C3, C4, C5, C6, C7, C8>(c1: Consumer<C1>, c2: Consumer<C2>, c3: Consumer<C3>, c4: Consumer<C4>, C5: Consumer<C5>, C6: Consumer<C6>, C7: Consumer<C7>, C8: Consumer<C8>): ResultComponent<C1, C2, C3, C4, C5, C6, C7, C8>
+export function consume<C1, C2, C3, C4, C5, C6, C7, C8, C9>(c1: Consumer<C1>, c2: Consumer<C2>, c3: Consumer<C3>, c4: Consumer<C4>, C5: Consumer<C5>, C6: Consumer<C6>, C7: Consumer<C7>, C8: Consumer<C8>, C9: Consumer<C9>): ResultComponent<C1, C2, C3, C4, C5, C6, C7, C8, C9>
+export function consume<C1, C2, C3, C4, C5, C6, C7, C8, C9, C10>(c1: Consumer<C1>, c2: Consumer<C2>, c3: Consumer<C3>, c4: Consumer<C4>, C5: Consumer<C5>, C6: Consumer<C6>, C7: Consumer<C7>, C8: Consumer<C8>, C9: Consumer<C9>, C10: Consumer<C10>): ResultComponent<C1, C2, C3, C4, C5, C6, C7, C8, C9, C10>
 export function consume(...consumers: Array<Consumer<any>>): any {
     const component = (props: Props): ReactNode =>
         consumers.reduce<Children>(
@@ -25,6 +31,6 @@ type Props = {
 
 type Children = (...contents: Array<any>) => ReactNode
 
-export type ResultComponent<C1=never, C2=never, C3=never, C4=never, C5=never> = ComponentType<{
-    children?: (a: C1, b: C2, c: C3, d: C4, e: C5) => ReactNode
+export type ResultComponent<C1=never, C2=never, C3=never, C4=never, C5=never, C6=never, C7=never, C8=never, C9=never, C10=never> = ComponentType<{
+    children?: (c1: C1, c2: C2, c3: C3, c4: C4, c5: C5, c6: C6, c7: C7, c8: C8, c9: C9, c10: C10) => ReactNode
 }>
--- src/provide.tsx
+++ src/provide.tsx
@@ -1,9 +1,15 @@
 import {ComponentType, default as React, Provider, ReactNode} from 'react'
 
-export function provide<C1>(p1: Provider<C1>): ResultComponent<[C1]>
-export function provide<C1, C2>(p1: Provider<C1>, p2: Provider<C2>): ResultComponent<[C1, C2]>
-export function provide<C1, C2, C3>(p1: Provider<C1>, p2: Provider<C2>,
-                                    p3: Provider<C3>): ResultComponent<[C1, C2, C3]>
+export function provide<C1>(c1: Provider<C1>): ResultComponent<[C1]>
+export function provide<C1, C2>(c1: Provider<C1>, c2: Provider<C2>): ResultComponent<[C1, C2]>
+export function provide<C1, C2, C3>(c1: Provider<C1>, c2: Provider<C2>, c3: Provider<C3>): ResultComponent<[C1, C2, C3]>
+export function provide<C1, C2, C3, C4>(c1: Provider<C1>, c2: Provider<C2>, c3: Provider<C3>, c4: Provider<C4>): ResultComponent<[C1, C2, C3, C4]>
+export function provide<C1, C2, C3, C4, C5>(c1: Provider<C1>, c2: Provider<C2>, c3: Provider<C3>, c4: Provider<C4>, C5: Provider<C5>): ResultComponent<[C1, C2, C3, C4, C5]>
+export function provide<C1, C2, C3, C4, C5, C6>(c1: Provider<C1>, c2: Provider<C2>, c3: Provider<C3>, c4: Provider<C4>, C5: Provider<C5>, C6: Provider<C6>): ResultComponent<[C1, C2, C3, C4, C5, C6]>
+export function provide<C1, C2, C3, C4, C5, C6, C7>(c1: Provider<C1>, c2: Provider<C2>, c3: Provider<C3>, c4: Provider<C4>, C5: Provider<C5>, C6: Provider<C6>, C7: Provider<C7>): ResultComponent<[C1, C2, C3, C4, C5, C6, C7]>
+export function provide<C1, C2, C3, C4, C5, C6, C7, C8>(c1: Provider<C1>, c2: Provider<C2>, c3: Provider<C3>, c4: Provider<C4>, C5: Provider<C5>, C6: Provider<C6>, C7: Provider<C7>, C8: Provider<C8>): ResultComponent<[C1, C2, C3, C4, C5, C6, C7, C8]>
+export function provide<C1, C2, C3, C4, C5, C6, C7, C8, C9>(c1: Provider<C1>, c2: Provider<C2>, c3: Provider<C3>, c4: Provider<C4>, C5: Provider<C5>, C6: Provider<C6>, C7: Provider<C7>, C8: Provider<C8>, C9: Provider<C9>): ResultComponent<[C1, C2, C3, C4, C5, C6, C7, C8, C9]>
+export function provide<C1, C2, C3, C4, C5, C6, C7, C8, C9, C10>(c1: Provider<C1>, c2: Provider<C2>, c3: Provider<C3>, c4: Provider<C4>, C5: Provider<C5>, C6: Provider<C6>, C7: Provider<C7>, C8: Provider<C8>, C9: Provider<C9>, C10: Provider<C10>): ResultComponent<[C1, C2, C3, C4, C5, C6, C7, C8, C9, C10]>
 export function provide(...providers: Array<Provider<any>>): any {
     const component = (props: Props) =>
         providers.reduce<ReactNode>(
